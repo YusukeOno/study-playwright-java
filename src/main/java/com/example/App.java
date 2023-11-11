343d40555a2538853892cc1d3f2c4b8d82a70e66
小野裕輔 <ono.yusuke@nsw-ss.co.jp>
2023-11-11 16:05:13 +0900

    Hello, Playwright

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,22 @@
 package com.example;
 
+import java.nio.file.Paths;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
 /**
- * Hello world!
+ * Hello Playwirght!
  *
  */
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    try (Playwright playwright = Playwright.create()) {
+      Browser browser = playwright.webkit().launch();
+      Page page = browser.newPage();
+      page.navigate("https://playwright.dev/");
+      page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+    }
   }
 }
