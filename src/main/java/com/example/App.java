e673803ab9894de9a71f8c5dabc451220ce333f4
小野裕輔 <ono.yusuke@nsw-ss.co.jp>
2023-11-25 14:08:29 +0900

    CheckStyle 10.12 to 9.3

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,22 +1,33 @@
 package com.example;
 
-import java.nio.file.Paths;
-
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import java.nio.file.Paths;
 
 /**
- * Hello Playwirght!
+ * Hello Playwirght.
  *
  */
-public class App {
-  public static void main(String[] args) {
+public final class App {
+  /**
+   * Main method.
+   *
+   * @param args Arguments
+   */
+  public static void main(final String[] args) {
     try (Playwright playwright = Playwright.create()) {
       Browser browser = playwright.webkit().launch();
       Page page = browser.newPage();
       page.navigate("https://playwright.dev/");
-      page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+      page.screenshot(
+          new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
     }
+  }
+
+  /**
+   * App.
+   */
+  private App() {
   }
 }
---
+++ src/main/java/com/example/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Example package.
+ */
+package com.example;
